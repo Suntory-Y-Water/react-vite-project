62f96675dc6040f823b2ea5b1c951b31cef5b0fc
sui <1202ayasnppk@gmail.com>
2024-06-03 07:33:15 +0900

    update: CDまで終了

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from '@/components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
   return (
     <>
@@ -16,9 +17,9 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-2xl font-bold'>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -12,6 +13,11 @@ export default defineConfig({
     // スナップショットの保存先を設定
     resolveSnapshotPath: (path, extension) => {
       return path.replace('/src/', '/__snapshots__/') + extension;
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
