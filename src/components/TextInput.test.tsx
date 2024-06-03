86c5d26c4619eaceed2c54c5747443fbf9877429
sui <1202ayasnppk@gmail.com>
2024-06-04 00:55:48 +0900

    snapショットテストコケたので再実行

--- src/components/TextInput.test.tsx
+++ src/components/TextInput.test.tsx
@@ -12,3 +12,8 @@ test('TextInput Component Test', async () => {
   await user.type(inputElement, 'Hello World');
   expect(screen.getByText('Entered Text: Hello World')).toBeInTheDocument();
 });
+
+test('スナップショットテスト', () => {
+  const { container } = render(<TextInput />);
+  expect(container).toMatchSnapshot();
+});
